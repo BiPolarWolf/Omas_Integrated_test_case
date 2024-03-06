3e3f0deab7182eadf9dcf14db25acb1eff14a548
snegirgik <snegirgik@gmail.com>
2024-03-06 07:44:28 +0500

    remove extra components

--- Animals/Animals/settings.py
+++ Animals/Animals/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv("SECRET_KEY"))
+SECRET_KEY = 'django-insecure-w+sg$38n8&gw&3516mxv)m(86m4_29b7485n#=*gwq*e41gj2@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.getenv('DEBUG')))
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
--- animals-react-app/src/components/appHeader/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-    return (
-        <div className="text-center">
-            <hr/>
-            <h1>Тестовое приложения связка React + DRF</h1>
-        </div>)
-}
-
-export default Header;
